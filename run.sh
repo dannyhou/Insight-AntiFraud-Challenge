38009a130f2203629d2edaaeb76cb0411705c315
git_hlyn <hlynursn@gmail.com>
2016-11-10 17:33:20 -0800

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env 
+python ./src/antifraud.py ./paymo_input/batch_payment.txt ./paymo_input/stream_payment.txt ./paymo_output/output1.txt ./paymo_output/output2.txt ./paymo_output/output3.txt
